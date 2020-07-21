bb2ea62588a777aeac7a0b5456d29cad57c3cbdd
Nathaniel Graff <nathaniel.graff@sifive.com>
2020-07-21 14:30:12 -0700

    Updates for Freedom Metal-next API
    Make identical to example-local-interrupt, except turn on hart-local
    interrupt vectoring with `-DMETAL_HLIC_VECTORED`.
    
    Signed-off-by: Nathaniel Graff <nathaniel.graff@sifive.com>

--- local-vector-interrupts.c
+++ local-vector-interrupts.c
@@ -1,127 +1,170 @@
 /* Copyright 2019 SiFive, Inc */
 /* SPDX-License-Identifier: Apache-2.0 */
 
-#include <stdio.h>
-#include <string.h>
-#include <metal/cpu.h>
-#include <metal/led.h>
 #include <metal/button.h>
+#include <metal/cpu.h>
+#include <metal/interrupt.h>
+#include <metal/led.h>
 #include <metal/switch.h>
+#include <stdio.h>
 
 #define RTC_FREQ	32768
 
-struct metal_cpu *cpu;
-struct metal_interrupt *cpu_intr, *tmr_intr;
-struct metal_led *led0_red, *led0_green;
-struct metal_interrupt *clint;
-int tmr_id, sip_irq;
+#ifndef metal_led_ld0red
+#define metal_led_ld0red metal_led_none
+#endif
+
+#ifndef metal_led_ld0green
+#define metal_led_ld0green metal_led_none
+#endif
+
+#ifndef metal_led_ld0blue
+#define metal_led_ld0blue metal_led_none
+#endif
+
+#define led0_red metal_led_ld0red
+#define led0_green metal_led_ld0green
+#define led0_blue metal_led_ld0blue
+
+#ifndef metal_button_btn0
+#define metal_button_btn0 metal_button_none
+#endif
+
+#ifndef metal_button_btn1
+#define metal_button_btn1 metal_button_none
+#endif
+
+#ifndef metal_button_btn2
+#define metal_button_btn2 metal_button_none
+#endif
+
+#ifndef metal_button_btn3
+#define metal_button_btn3 metal_button_none
+#endif
+
+#define but0 metal_button_btn0
+#define but1 metal_button_btn1
+#define but2 metal_button_btn2
+#define but3 metal_button_btn3
+
+#ifndef metal_switch_sw3
+#define metal_switch_sw3 metal_switch_none
+#endif
+
+#define swch3 metal_switch_sw3
+
+int sw_status = 99;
 
 void display_instruction (void) {
     printf("\n");
     printf("SIFIVE, INC.\n!!\n");
     printf("\n");
-    printf("Coreplex IP Eval Kit 'clint-vector-interrupts' Example.\n\n");
-    printf("IRQ 3 (SIP) is enabled as external interrupt source.\n");
-    printf("A 10s debounce timer is used to trigger and clear SIP interupt.\n");
+    printf("E31/E51 Coreplex IP Eval Kit 'local-interrupt' Example.\n\n");
+    printf("Buttons 0-3 and Switch 3 are enabled as local interrupt sources.\n");
+    printf("A 1s debounce timer is used between these interupts.\n");
+    printf("Pressing Buttons 0-3 toggle LEDs, while turn ON Switch 3 to exit.\n");
     printf("\n");
 }
 
-void timer_isr (int id, void *data) {
-    printf("**** Lets trigger clint software interrupt ****\n");
-    metal_interrupt_set(clint, sip_irq);
+void metal_riscv_cpu_intc_mtip_handler(void) {
+    // Disable Timer interrupt
+    metal_cpu_disable_timer_interrupt();
+    printf("Awaken\n");
+
+    // Enable local IRQs
+    metal_button_clear_interrupt(but0);
+    metal_button_enable_interrupt(but0);
+    metal_button_clear_interrupt(but1);
+    metal_button_enable_interrupt(but1);
+    metal_button_clear_interrupt(but2);
+    metal_button_enable_interrupt(but2);
+    metal_button_clear_interrupt(but3);
+    metal_button_enable_interrupt(but3);
+    metal_switch_enable_interrupt(swch3);
 }
 
-void sip_isr(int id, void *data) {
-    printf("Got the SIP interrupt!\n");
-    metal_interrupt_clear(clint, sip_irq);
-    printf("Lets clear and re-arm timer another 10 seconds.\n");
-    metal_led_toggle(led0_green);
+void debounce (void) {
+    struct metal_cpu cpu = metal_cpu_get(metal_cpu_get_current_hartid());
+
+    // Disable local IRQs
+    metal_button_disable_interrupt(but0);
+    metal_button_disable_interrupt(but1);
+    metal_button_disable_interrupt(but2);
+    metal_button_disable_interrupt(but3);
+    metal_switch_disable_interrupt(swch3);
+
+    printf("Sleep for %d more times\n", sw_status--);
+    metal_cpu_set_mtimecmp(cpu, metal_cpu_get_mtime(cpu) + RTC_FREQ);
+
+    // Enable Timer interrupt
+    metal_cpu_enable_timer_interrupt();
+}
+
+void metal_button_btn0_interrupt_handler (void) {
+    printf("Button 0 was pressed. Toggle Red LED.\n");
     metal_led_toggle(led0_red);
-    metal_cpu_set_mtimecmp(cpu, metal_cpu_get_mtime(cpu) + 10*RTC_FREQ);
+    debounce();  
+}
+void metal_button_btn1_interrupt_handler (void) {
+    printf("Button 1 was pressed. Toggle Green LED.\n");
+    metal_led_toggle(led0_green);
+    debounce();
+}
+void metal_button_btn2_interrupt_handler (void) {
+    printf("Button 2 was pressed. Toggle Blue LED.\n");
+    metal_led_toggle(led0_blue);
+    debounce();
+}
+void metal_button_btn3_interrupt_handler (void) {
+    printf("Button 3 was pressed. No LED change!\n");
+    debounce();
+}
+
+void metal_switch_sw3_interrupt_handler(void) {
+    metal_switch_disable_interrupt(swch3);
+    printf("Switch 3 is on!\n");
+    sw_status = 0;
+    printf("Time to exit!\n");
 }
 
 int main (void)
 {
-    int rc;
-
     // Lets get start with getting LEDs and turn only RED ON
-    led0_red = metal_led_get_rgb("LD0", "red");
-    led0_green = metal_led_get_rgb("LD0", "green");
-    if ((led0_red == NULL) || (led0_green == NULL)) {
-        printf("Abort. At least one of LEDs is null.\n");
-        return 1;
-    }
     metal_led_enable(led0_red);
     metal_led_enable(led0_green);
+    metal_led_enable(led0_blue);
     metal_led_on(led0_red);
     metal_led_off(led0_green);
+    metal_led_off(led0_blue);
  
-    // Lets get the CPU and and its interrupt
-    cpu = metal_cpu_get(metal_cpu_get_current_hartid());
-    if (cpu == NULL) {
-        printf("Abort. CPU is null.\n");
-        return 2;
-    }
-    cpu_intr = metal_cpu_interrupt_controller(cpu);
-    if (cpu_intr == NULL) {
-        printf("Abort. CPU interrupt controller is null.\n");
-        return 3;
-    }
-    metal_interrupt_init(cpu_intr);
-
-    // Setup Timer interrupt
-    tmr_intr = metal_cpu_timer_interrupt_controller(cpu);
-    if (tmr_intr == NULL) {
-        printf("Abort. TIMER interrupt controller is  null.\n");
-        return 4;
-    }
-    metal_interrupt_init(tmr_intr);
-    tmr_id = metal_cpu_timer_get_interrupt_id(cpu);
-    rc = metal_interrupt_register_handler(tmr_intr, tmr_id, timer_isr, cpu);
-    if (rc < 0) {
-        printf("Failed. TIMER interrupt handler registration failed\n");
-        return (rc * -1);
-    }
-
-    // Lets get a CLINT interrupt controller explicitly
-    clint = metal_interrupt_get_controller(METAL_CLINT_CONTROLLER,
-                                           metal_cpu_get_current_hartid());
-    if (clint == NULL) {
-        printf("Exit. This example need a clint interrupt controller.\n");
-        return 0;
-    }
-    metal_interrupt_init(clint);
-
-    // Lets set CLINT in Vector mode. Note this must be done AFTER init!!
-    metal_interrupt_set_vector_mode(clint, METAL_VECTOR_MODE);
-
-    sip_irq = 7;
-    rc = metal_interrupt_register_handler(clint, sip_irq, sip_isr, NULL);
-    if (rc < 0) {
-        printf("SW interrupt handler registration failed\n");
-        return (rc * -1);
-    }
-
-    // Lets enable Switches interrupts
-    if (metal_interrupt_enable(clint, sip_irq) == -1) {
-        printf("SW interrupt enable failed\n");
+    // Lets enable the Buttons and Switch interrupts
+    if (metal_button_enable_interrupt(but0) == -1) {
+        printf("BTN0 interrupt enable failed\n");
         return 5;
     }
-    // Set timeout of 10s, and enable timer interrupt
-    metal_cpu_set_mtimecmp(cpu, metal_cpu_get_mtime(cpu) + 10*RTC_FREQ);
-    metal_interrupt_enable(tmr_intr, tmr_id);
+    if (metal_button_enable_interrupt(but1) == -1) {
+        printf("BTN1 interrupt enable failed\n");
+        return 5;
+    }
+    if (metal_button_enable_interrupt(but2) == -1) {
+        printf("BTN2 interrupt enable failed\n");
+        return 5;
+    }
+    if (metal_button_enable_interrupt(but3) == -1) {
+        printf("BTN3 interrupt enable failed\n");
+        return 5;
+    }
+    if (metal_switch_enable_interrupt(swch3) == -1) {
+        printf("SW3 interrupt enable failed\n");
+        return 5;
+    }
+
+    metal_cpu_enable_interrupts();
 
     display_instruction();
 
-    // Lastly CPU interrupt
-    if (metal_interrupt_enable(cpu_intr, 0) == -1) {
-        printf("CPU interrupt enable failed\n");
-        return 6;
+    while (sw_status) {
     }
 
-    while (1) {
-        __asm__ volatile ("wfi");
-    }
-
-    return 0;
+    return sw_status;
 }
